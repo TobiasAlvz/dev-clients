d3bb26d0c3e883baee03e116940c287efaa62c3f
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-01-19 16:01:47 -0300

    up

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,16 +1,32 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {},
-  preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/main/index.js'),
+          tray: resolve(__dirname, 'src/main/tray.js')
+        },
+        output: {
+          dir: 'out/main'
+        }
       }
-    },
-    plugins: [react()]
+    }
+  },
+
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+
+  renderer: {
+    plugins: [
+      react({
+        jsxRuntime: 'automatic'
+      })
+    ]
   }
 })
---
+++ src/main/tray.js
@@ -0,0 +1,6 @@
+const { Menu, Tray, nativeImage, app } = require('electron')
+const path = require('node:path')
+
+let tray = null
+
+
