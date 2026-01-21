c58a4c9b585eb175804dbf500bdabbf291d0f149
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-01-21 16:39:44 -0300

    up

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,10 +1,14 @@
 import Routes from './Routes'
 import './styles/app.css'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export default function App() {
   return (
-    <div className="app-container">
-      <Routes />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app-container">
+        <Routes />
+      </div>
+    </QueryClientProvider>
   )
 }
--- src/renderer/src/pages/Home.jsx
+++ src/renderer/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
-import Container from '../components/Container'
 import '../styles/home.css'
+import { useQuery } from '@tanstack/react-query'
 
 export default function Home() {
   const { data, isLoading, error } = useQuery({
